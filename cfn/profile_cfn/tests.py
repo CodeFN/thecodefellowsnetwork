27061481140cb4316aeb77c4b5aca57cfd43d094
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-13 16:17:52 -0800

    tests for profile.

--- cfn/profile_cfn/tests.py
+++ cfn/profile_cfn/tests.py
@@ -1,3 +1,62 @@
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
+from django.contrib.auth.models import User
+from profile_cfn.models import ProfileCfn
+from profile_cfn.views import ProfileCfnView
+import factory
 
-# Create your tests here.
+
+class UserFactory(factory.django.DjangoModelFactory):
+    """User factory for testing."""
+
+    class Meta:
+        """Model meta."""
+
+        model = User
+
+    username = factory.Sequence(lambda n: "User{}".format(n))
+    email = factory.LazyAttribute(
+        lambda x: "{}@imager.com".format(x.username.replace(" ", ""))
+    )
+
+
+class BackendTests(TestCase):
+    """User/profile backend test runner."""
+
+    def setUp(self):
+        """Set up for backend test runner."""
+        self.users = [UserFactory.create() for i in range(5)]
+
+    def test_profile_is_made_when_user_is_saved(self):
+        """Test profile created when user is registered."""
+        self.assertTrue(ProfileCfn.objects.count() == 5)
+
+    def test_profile_is_associated_with_users(self):
+        """Test profile has correct user instance."""
+        profile = ProfileCfn.objects.first()
+        username = self.users[0].username
+        self.assertTrue(profile.user.username == username)
+
+
+class FrontendTests(TestCase):
+    """User/profile frontend test runner."""
+
+    def setUp(self):
+        """Set up for frontend fest runner."""
+        self.client = Client()
+        self.request = RequestFactory()
+
+    def add_user(self):
+        """Make a user and return his profile."""
+        user = UserFactory.create()
+        user.username = 'test_user'
+        user.set_password('testpassword')
+        user.save()
+        return user
+
+    def test_profile_view_returns_status_ok(self):
+        """Test profile view returns status code 200."""
+        test_user = self.add_user()
+        import pdb; pdb.set_trace()
+        self.client.force_login(test_user)
+        response = self.client.get("/profile")
+        self.assertTrue(response.status_code == 200)
