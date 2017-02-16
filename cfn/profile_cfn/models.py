34879c2459056d5c9662f00469c4baad44d7cb59
William Benjamin Shields <ben@Williams-MacBook-Pro.local>
2017-02-15 19:27:05 -0800

    try except models.

--- cfn/profile_cfn/models.py
+++ cfn/profile_cfn/models.py
@@ -33,12 +33,14 @@ def make_user_profile(sender, instance, **kwargs):
     """Create and save a profile when a user is created."""
     if kwargs["created"]:
         profile = ProfileCfn(user=instance)
-        json_list = []
-        req = requests.get('https://api.github.com/users/' + str(profile))
-        content = req.content.decode('utf-8')
-        json_list.append(json.loads(content))
-        if json_list[0]['avatar_url']:
-            profile.avatar_url = json_list[0]['avatar_url']
-        profile.about = json_list[0]['bio']
+        try:
+            json_list = []
+            req = requests.get('https://api.github.com/users/' + str(profile))
+            content = req.content.decode('utf-8')
+            json_list.append(json.loads(content))
+            if json_list[0]['avatar_url']:
+                profile.avatar_url = json_list[0]['avatar_url']
+            profile.about = json_list[0]['bio']
+        except:
+            pass
         profile.save()
-
