2777a54fd02d6350c731fb81257c0e07108e2733
William Benjamin Shields <bshields23@gmail.com>
2017-03-23 22:03:26 -0700

    Update settings.py

--- cfn/cfn/settings.py
+++ cfn/cfn/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'ec2-52-35-131-204.us-west-2.compute.amazonaws.com',
